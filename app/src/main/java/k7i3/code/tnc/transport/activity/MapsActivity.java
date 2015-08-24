5db5ff0ec0310dc6fe862bdf36ef92a958a4e839
k7i3 <klim.yakovlev@gmail.com>
2015-08-24 18:21:10 +0500

    wip: UI / make status bar transparent programmatically only for MapsActivity

--- app/src/main/java/k7i3/code/tnc/transport/activity/MapsActivity.java
+++ app/src/main/java/k7i3/code/tnc/transport/activity/MapsActivity.java
@@ -1,10 +1,11 @@
 package k7i3.code.tnc.transport.activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,14 +16,13 @@ import k7i3.code.tnc.transport.R;
 
 public class MapsActivity extends BaseActivity {
 
+    private GoogleMap googleMap; // Might be null if Google Play services APK is not available.
+    private FloatingActionButton fabFilterOptions;
+
     @Override
     protected int getLayoutResource() {
         return R.layout.activity_maps;
     }
-
-    private GoogleMap googleMap; // Might be null if Google Play services APK is not available.
-    private FloatingActionButton fabFilterOptions;
-    private FloatingActionButton fabCurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +38,10 @@ public class MapsActivity extends BaseActivity {
     }
 
     private void initInstances() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         fabFilterOptions = (FloatingActionButton) findViewById(R.id.fabFilterOptions);
         fabFilterOptions.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,15 +49,6 @@ public class MapsActivity extends BaseActivity {
                 startActivity(new Intent(getBaseContext(), RoutesActivity.class));
             }
         });
-
-//                Snackbar.make(rootLayout, "fabFilterOptions", Snackbar.LENGTH_SHORT)
-//                        .setAction("Undo", new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View v) {
-//
-//                            }
-//                        })
-//                        .show();
     }
 
     /**
