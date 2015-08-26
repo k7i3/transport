7bab65eee3c94795686a4e2ba737a686cfde9ee2
k7i3 <klim.yakovlev@gmail.com>
2015-08-26 17:58:29 +0500

    wip: UI / refactor MapsActivity (getMapAsync() instead of getMap())

--- app/src/main/java/k7i3/code/tnc/transport/activity/MapsActivity.java
+++ app/src/main/java/k7i3/code/tnc/transport/activity/MapsActivity.java
@@ -8,13 +8,15 @@ import android.view.View;
 import android.view.WindowManager;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import k7i3.code.tnc.transport.R;
 
-public class MapsActivity extends BaseActivity {
+public class MapsActivity extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap googleMap; // Might be null if Google Play services APK is not available.
     private FloatingActionButton routesFAB;
@@ -27,14 +29,13 @@ public class MapsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setUpMapIfNeeded();
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
         initInstances();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
     }
 
     private void initInstances() {
@@ -54,41 +55,15 @@ public class MapsActivity extends BaseActivity {
         });
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #googleMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (googleMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (googleMap != null) {
-                setUpMap();
-            }
-        }
-    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #googleMap} is not null.
-     */
-    private void setUpMap() {
+        googleMap.setMyLocationEnabled(true);
+        UiSettings uiSettings = googleMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+
         googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
